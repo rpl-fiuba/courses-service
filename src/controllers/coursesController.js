1e717ab72905cc35e093ce3b6ab494b613cd4c0b
diegokim <diego.f.kim@gmail.com>
2019-10-17 01:50:04 -0300

    linter

--- src/controllers/coursesController.js
+++ src/controllers/coursesController.js
@@ -1,16 +1,15 @@
 const createError = require('http-errors');
 const expressify = require('expressify')();
 const logger = require('../utils/logger.js');
-const coursesService = require('../services/coursesService')
+const coursesService = require('../services/coursesService');
 
 /**
  * Get courses.
  */
 const getCourses = async (req, res) => {
-  let page = req.query.page;
-  let limit = req.query.limit;
+  const { page, limit } = req.query;
 
-  let courses = await coursesService.getCourses({page, limit})
+  const courses = await coursesService.getCourses({ page, limit });
 
   return res.status(200).json(courses);
 };
@@ -18,39 +17,40 @@ const getCourses = async (req, res) => {
 const addCourse = async (req, res) => {
 
   const { context } = req;
-  const { name, description} = req.body;
+  const { name, description } = req.body;
   const { email, id } = context.googleProfile;
 
   // TODO: validar el rol del usuario.
 
-  // Agregar curso  
   if (!name || !description) {
     return Promise.reject(createError.BadRequest('name or description have not been provided'));
   }
 
   const creatorId = id;
-  
   await coursesService.addCourse({
-    name, 
+    name,
     description,
     creatorId,
   });
 
   return res.status(201).json({});
-}
+};
 
 const updateCourse = async (req, res) => {
   return res.status(200).json({});
-}
+};
+
 const updateCourseUsers = async (req, res) => {
   return res.status(200).json({});
-}
+};
+
 const getCourseUsers = async (req, res) => {
   return res.status(200).json({});
-}
+};
+
 const deleteCourse = async (req, res) => {
   return res.status(200).json({});
-}
+};
 
 module.exports = expressify({
   getCourses,
--- src/databases/coursesDb.js
+++ src/databases/coursesDb.js
@@ -9,57 +9,55 @@ const knex = require('knex')(configs.db); // eslint-disable-line
  *
  */
 const getCourses = async ({ page, limit }) => {
-  
+
   let pageSize = configs.coursesConfig.pageSize;
+
   return knex('courses')
     .select()
     .returning('*')
     .then(processDbResponse)
     .then((response) => {
-      console.log(response)
+      console.log(response);
       if (!response) {
         throw new createError.NotFound('Courses not found');
       }
       return response;
     })
-    .catch(console.log)
+    .catch(console.log);
 };
 
-const newCourse = ({trx, name, description}) => {
+const newCourse = ({ trx, name, description }) => {
   const id = name.toLowerCase().replace(' ', '');
-  return knex('courses')
-    .transacting(trx)
-    .insert({
-      id,
-      name,
-      description,
-    });
-}
-
-const createCourseCreator = ({trx, courseId, creatorId}) => {
-  return knex('course_users')
-    .transacting(trx)
-    .insert({
-      userId: creatorId,
-      courseId,
-      role: 'admin'
-    })
-}
-
-const addCourse = async ({ courseId, name, description, creatorId }) => {
-
-  knex.transaction()
-    .then(trx => newCourse({trx, name, description}))
-    .then(trx => createCourseCreator({trx, courseId}))
-    .then(trx.commit)
-    .catch(trx.rollback);// TODO: handle this.
-}
+  return trx.insert({
+    id,
+    name,
+    description,
+  }).into('courses');
+};
 
 
+const createCourseCreator = ({
+  trx,
+  courseId,
+  creatorId,
+}) => trx
+  .insert({
+    userId: creatorId,
+    courseId,
+    role: 'admin'
+  }).into('course_users');
 
 
-
-
+const addCourse = async ({
+  courseId,
+  name,
+  description,
+  creatorId,
+}) => {
+  const trx = await knex.transaction();
+  newCourse({ trx, name, description })
+    .then(() => createCourseCreator({ trx, courseId, creatorId }));
+};
 
 module.exports = {
   getCourses
