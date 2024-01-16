c49f61e859d1bde035c18aaece08950c8a9216a9
lorenzolgz <lgimenez@fi.uba.ar>
2024-01-15 22:24:26 -0300

    Add dummy / route

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(requestLoggerMiddleware);
 
 // Routes
 router.get('/ping', (req, res) => statusController.ping(req, res));
+router.get('/', (req, res) => statusController.ping(req, res));
 
 router.use(initialMiddleware);
 router.use(authMiddleware);
